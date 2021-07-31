512a8758b1d673e6d28941e4cd9258cff1fba22b
Leomotors (Windows) <59821765+Leomotors@users.noreply.github.com>
2021-07-31 18:18:39 +0700

    drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final random = Random();
   final _salimAPIUrl = "https://watasalim.vercel.app/api/quotes";
   List<Quote> quotes = [];
+  String currentQuote = "";
 
   @override
   void initState() {
@@ -41,9 +42,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> getSalimQuote() async {
+    if (quotes.length > 0) return;
     Uri _salimUri = Uri.parse(_salimAPIUrl);
     var response = await http.read(_salimUri);
     quotes = salimQuoteFromJson(response).quotes;
+  }
+
+  void randomText() {
+    currentQuote = quotes[random.nextInt(quotes.length)].body;
   }
 
   @override
@@ -51,16 +57,52 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Daily Salim Quote"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                randomText();
+              });
+            },
+            icon: Icon(Icons.replay),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: getSalimQuote(),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return Text("${quotes[random.nextInt(quotes.length)]}");
+            return Center(
+              child: Text(
+                "$currentQuote",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
           } else {
             return LinearProgressIndicator();
           }
         },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text(
+                "เมนูของพวกชังชาติ",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
